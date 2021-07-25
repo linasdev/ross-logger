a08b84ad362c9ac634dbc367c788485ded75159a
Linas Nikiperavičius <linas@linasdev.com>
2021-07-25 21:34:17 +0300

    Add macros for easier logging

--- src/ross_logger.rs
+++ src/ross_logger.rs
@@ -1,6 +1,36 @@
 use cortex_m::iprint;
 use stm32f1xx_hal_bxcan::pac::ITM;
 
+#[macro_export]
+macro_rules! log_debug {
+    ($logger:expr, $fmt:expr) => {
+        logger.log_debug($fmt);
+    };
+    ($logger:expr, $fmt:expr, $($arg:tt)*) => {
+        logger.log_debug(format!($fmt, $($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! log_warning {
+    ($logger:expr, $fmt:expr) => {
+        logger.log_warning($fmt);
+    };
+    ($logger:expr, $fmt:expr, $($arg:tt)*) => {
+        logger.log_warning(format!($fmt, $($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($logger:expr, $fmt:expr) => {
+        logger.log_error($fmt);
+    };
+    ($logger:expr, $fmt:expr, $($arg:tt)*) => {
+        logger.log_error(format!($fmt, $($arg)*));
+    };
+}
+
 pub enum RossLogLevel {
     Debug,
     Warning,
